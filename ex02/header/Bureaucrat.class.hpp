8f6a7ff3f7cb2b16a47df30320c80828e8b0f717
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-08 20:55:11 +0100

    bureau hpp

---
+++ ex02/header/Bureaucrat.class.hpp
@@ -0,0 +1,55 @@
+#ifndef BUREAUCRAT_CLASS_HPP
+# define BUREAUCRAT_CLASS_HPP
+
+#pragma once
+#include <iostream>
+#include "AForm.class.hpp"
+
+class AForm;
+
+class Bureaucrat
+{
+	private:
+
+		const std::string	name;
+		int					grade;
+		static const int			max_grade = 1;
+		static const int			min_grade = 150;
+		
+	public:
+
+		int			getGrade(void) const;
+		std::string	getName(void) const;
+		
+		void		incrementGrade(void);
+		void		decrementGrade(void);
+		void		signForm(AForm &form);
+
+		Bureaucrat(void);
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(Bureaucrat const &src);
+		Bureaucrat&	operator=(Bureaucrat const &assign);
+		~Bureaucrat(void);
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too low");
+				}
+		};
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade too high");
+				}
+		};
+};
+
+std::ostream&	operator<<(std::ostream &o, Bureaucrat const &i);
+
+#endif
