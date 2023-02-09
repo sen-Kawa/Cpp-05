762ac0bc6c64b0a83566342f20320fd032e427cf
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-09 19:56:00 +0100

    adding cpp robo

---
+++ ex02/src/RobotomyRequestForm.class.cpp
@@ -0,0 +1,47 @@
+#include "../header/RobotomyRequestForm.class.hpp"
+#include <cstdlib>
+#include <fstream>
+
+std::string RobotomyRequestForm::getTarget(void) const
+{
+	return (this->target);
+}
+
+void RobotomyRequestForm::execute(const Bureaucrat &executor) const
+{
+	(void) executor;
+	std::cout << "Some drilling noises." << std::endl;
+	if ((std::rand() % 2) == 1) 
+		std::cout << target << " has been robotomized succesfully." << std::endl;
+	else if ((std::rand() % 2) == 0) 
+		std::cout << target << " robotomization failed." << std::endl;
+	return ;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(void) : AForm("Robotomy request form", 72, 45), target("space")
+{
+	return ;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string target) : AForm("Robotomy request form", 72, 45), target(target)
+{
+	return ;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &src) : AForm(), target(src.target)
+{
+	*this = src;
+	return ;
+}
+
+RobotomyRequestForm &	RobotomyRequestForm::operator=(const RobotomyRequestForm &assign)
+{
+	this->target = assign.getTarget();
+	return (*this);
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(void)
+{
+	return ;
+}
+
