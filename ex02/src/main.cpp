f39e0967ecf9f9097bfa6ad7739a6778f996e8bc
olin_tp_ar <universalexperimental@protonmail.com>
2023-02-09 19:55:49 +0100

    robotomy tests

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,4 +1,5 @@
 #include "../header/Bureaucrat.class.hpp"
+#include "../header/RobotomyRequestForm.class.hpp"
 #include "../header/AForm.class.hpp"
 #include "../header/ShrubberyCreationForm.class.hpp"
 #include <exception>
@@ -31,6 +32,34 @@ void shrubberyTest(void)
 	return ;
 }
 
+void roboTest(void)
+{
+	int	i = 0;
+	int	grades[3] = {78, 60, 38};
+	std::cout
+	<< "** ROBOTOMY TEST **\n" << std::endl;
+
+	while (i < 3)
+	{
+		std::cout << "\n  >> TEST " << i << "\n" << std::endl;
+
+		RobotomyRequestForm		robo("Grape");	
+		Bureaucrat				bureau("Bureau", grades[i]);
+		std::cout << robo << std::endl;
+		std::cout << bureau << std::endl;
+
+		std::cout << "\n...signing...\n" << std::endl;
+		bureau.signForm(robo);
+
+		std::cout << "\n...executing...\n" << std::endl;
+		bureau.executeForm(robo);
+		i++;
+	}
+	std::cout
+	<< "\n** END OF ROBOTOMY TEST **\n" << std::endl;
+	return ;
+}
+
 void menu(void)
 {
 	std::cout
@@ -53,6 +82,8 @@ int main(void)
 		getline(std::cin, test);
 		if (test == "S")
 			shrubberyTest();
+		if (test == "R")
+			roboTest();
 	}
 	return (0);
 }
